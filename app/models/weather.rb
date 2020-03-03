5823dba1fb09fb593d1a821fd3b34482436daa5e
IiyamaMakoto <m.iiyama@gmail.com>
2020-03-03 23:48:17 +0900

    Delete weather.rb

--- app/models/weather.rb
+++
@@ -1,8 +0,0 @@
-# class Weather < ActiveHash::Base
-#   self.data = [
-#     {id: 1, name: "にほんばれ"},
-#     {id: 2, name: "あめ"},
-#     {id: 3, name: "すなあらし"},
-#     {id: 4, name: "あられ"}
-#   ]
-# end
