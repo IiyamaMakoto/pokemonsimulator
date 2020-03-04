f19eb2c572f663d18ada4d286c5edd31d0f2bb8c
IiyamaMakoto <m.iiyama@gmail.com>
2020-03-04 17:35:17 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,6 +21,18 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
+before "deploy:assets:precompile", "deploy:yarn_install"
+namespace :deploy do
+  desc "Run rake yarn install"
+  task :yarn_install do
+    on roles(:web) do
+      within release_path do
+        execute("cd #{release_path} && yarn install --silent --no-progress --no-audit --no-optional")
+      end
+    end
+  end
+end
+
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
